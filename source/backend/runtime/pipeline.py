4995e363523137fb79683b6e9069f709dd995b8e
Shin Hye Sun <ryanjohnswanson@icloud.com>
2025-10-03 22:40:13 -0400

    rm useless crap

--- source/backend/runtime/pipeline.py
+++
@@ -1,8 +0,0 @@
-﻿# ============================================================
-#  Project: Soke
-#  File: pipeline.py
-#  Author: Ishura
-#
-#  This file (pipeline.py) is part of the Soke project.
-#  Â© 2025 Ishura. All rights reserved.
-# ============================================================
